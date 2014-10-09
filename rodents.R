bd7b9b9845dfd66cace740bf121fc62436f12961
David J. Harris <davharris@users.noreply.github.com>
2014-10-09 14:56:27 -0700

    Update rodents.R

--- rodents.R
+++ rodents.R
@@ -4,9 +4,17 @@ head(rodents)
 
 plot(table(rodents$yr))
 
-par(mfrow = c(3, 4))
+par(mfrow = c(3, 4))  # Break the plotting area into a 3x4 grid
+
+# Take each plot from the first to the 24th...
 for(plot_number in 1:24){
-  counts = rodents$yr[rodents$plot == plot_number]
-  plot(table(counts), main = plot_number)
+  # Note whether the observations occurred in the current plot_number of interest
+  in_plot = rodents$plot == plot_number
+  
+  # Tally up the number of rows in the plot of interest that happened in each year
+  tally = table(rodents$yr[in_plot])
+  
+  # Plot the tally
+  plot(tally, main = plot_number)
 }
 
