a941a7d2a9d9cfc5c2e9a7ae9690035e250b8d3e
David J. Harris <davharris@users.noreply.github.com>
2014-10-09 10:46:14 -0700

    Update taxize.R

--- taxize.R
+++ taxize.R
@@ -1,4 +1,9 @@
 library(taxize)
+
+
 mynames <- c("Helianthus annuus", "Pinus contort", "Poa anua", "Abis magnifica",
     "Rosa california", "Festuca arundinace", "Sorbus occidentalos","Madia sateva")
 tnrs(query = mynames, source = "iPlant_TNRS")[ , -c(5:7)]
+
+
+comm2sci(c("American Crow", "Western Scrub-jay", "Yellow-billed Magpie"))
