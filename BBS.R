a1d0dfcc18e52446cb05a8bae67c7a9a62d874e6
David J. Harris <davharris@users.noreply.github.com>
2014-10-16 11:13:57 -0700

    Update BBS.R

--- BBS.R
+++ BBS.R
@@ -10,7 +10,37 @@ download.file("ftp://ftpext.usgs.gov/pub/er/md/laurel/BBS/DataFiles/Routes.zip",
 routes <- read.csv(unzip(temp))
 unlink(temp)
 
+# Link the Route IDs between the two data sets
 data = merge(finch, routes)
 
-plot(data[data$Aou == 5190, c("Longi", "Lati")])
+latlon = data[data$Aou == 5190, c("Longi", "Lati")]
+
+# Plot the observations for species 5190 on a simple map
+plot(latlon)
+
+
+library(maps)
+library(mapdata)
+library(mapproj)
+
+png("map.png", width = 3200, height = 2800, res = 1000)
+par("mai" = c(0, 0, 0, 0))
+map(
+  database = "worldHires", 
+  proj = "albers", 
+  param = c(29.5, 45.5),
+  interior = TRUE,
+  ylim = c(25, 75),
+  xlim = c(-167, -53),
+  resolution = 0.2,
+  wrap = TRUE,
+  col = "gray",
+  mar = c(0, 0, 0, 0),
+  myborder = 0,
+  orientation = c(95, -102, 0),
+  lwd = 1
+)
+points(mapproject(latlon$Longi, latlon$Lati), pch = 16, cex = .15, col = "red")
+dev.off()
+
 
