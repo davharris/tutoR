9f1831d6a9d30259c52b1ece49753bd2e075bea8
David J. Harris <davharris@users.noreply.github.com>
2014-10-16 10:55:16 -0700

    Update BBS.R

--- BBS.R
+++ BBS.R
@@ -1,8 +1,10 @@
+# Get the data on finch sightings by Route ID
 temp <- tempfile()
 download.file("ftp://ftpext.usgs.gov/pub/er/md/laurel/BBS/DataFiles/Species/Finches.zip",temp)
 finch <- read.csv(unzip(temp))
 unlink(temp)
 
+# Get the data on where the routes are
 temp <- tempfile()
 download.file("ftp://ftpext.usgs.gov/pub/er/md/laurel/BBS/DataFiles/Routes.zip",temp)
 routes <- read.csv(unzip(temp))
